2eee10e1add0c702e52bf22cf5b9300b32001e1c
aaron <winn403@hotmail.com>
2017-11-16 01:54:51 +0000

    update

--- memoryjs.js
+++ memoryjs.js
@@ -2,6 +2,42 @@ var cards = [
     {
         name: "picture1",
         img: "memorypics/pic1.jpg",
-        id: 1,
+        id: pic1,
     },
+    {
+        name: "picture2",
+        img: "memorypics/pic2.jpg",
+        id: pic2,
+    },
+    {
+        name: "picture3",
+        img: "memorypics/pic3.jpg",
+        id: pic3,
+    },
+    {
+        name: "picture4",
+        img: "memorypics/pic4.jpg",
+        id: pic4,
+    },
+    {
+        name: "picture5",
+        img: "memorypics/pic5.jpg",
+        id: pic5,
+    },
+    {
+        name: "picture6",
+        img: "memorypics/pic6.jpg",
+        id: pic6,
+    },
+    {
+        name: "picture7",
+        img: "memorypics/pic7.jpg",
+        id: pic7,
+    },
+    {
+        name: "picture8",
+        img: "memorypics/pic8.jpg",
+        id: pic8,
+    },
+   
     
