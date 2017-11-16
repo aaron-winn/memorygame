1a6bbe25b99bb98ce5d899a012b152640e7c251f
aaron <winn403@hotmail.com>
2017-11-16 02:08:26 +0000

    shs

--- memoryjs.js
+++ memoryjs.js
@@ -1,43 +1,50 @@
+
 var cards = [
     {
         name: "picture1",
         img: "memorypics/pic1.jpg",
-        id: pic1,
+        
     },
     {
         name: "picture2",
         img: "memorypics/pic2.jpg",
-        id: pic2,
+        
     },
     {
         name: "picture3",
         img: "memorypics/pic3.jpg",
-        id: pic3,
+        
     },
     {
         name: "picture4",
         img: "memorypics/pic4.jpg",
-        id: pic4,
+        
     },
     {
         name: "picture5",
         img: "memorypics/pic5.jpg",
-        id: pic5,
+        
     },
     {
         name: "picture6",
         img: "memorypics/pic6.jpg",
-        id: pic6,
+        
     },
     {
         name: "picture7",
         img: "memorypics/pic7.jpg",
-        id: pic7,
+        
     },
     {
         name: "picture8",
         img: "memorypics/pic8.jpg",
-        id: pic8,
+        
     },
+]
+
+// fetch reference to the table
+// loop through it and find all the img tags and save them to an array
+
+// save 
    
     
